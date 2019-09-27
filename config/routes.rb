ff754a5f18d34eb452f88814790e74cf317f10db
Kemal C <ddeveloperr@gmail.com>
2019-09-27 22:28:46 +0200

    Add routes for the new resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Add routes for the new resources
+  jsonapi_resources :contacts
+  jsonapi_resources :phone_numbers
+
 end
