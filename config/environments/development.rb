35e9b6bb27da8637a9591cc734fb030410f7f69a
Kemal C <ddeveloperr@gmail.com>
2019-09-27 20:15:01 +0200

    Coonfigure dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,11 +6,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Do not eager load code on boot.
-  config.eager_load = false
+  # # Eager load code on boot so JSONAPI-Resources resources are loaded and processed globally
+  config.eager_load = true
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
